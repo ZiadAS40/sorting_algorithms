d2d280bf49b3152fbb61815fcc3264cbf7fff28d
ZiadAS40 <zead4036@gmail.com>
2024-05-01 00:48:41 +0300

    adding the last task

---
+++ 3-quick_sort.c
@@ -0,0 +1,72 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Entry point for the quicksort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size <= 1)
+		return;
+	recursive_sort(array, 0, size - 1, size);
+}
+
+/**
+ * recursive_sort - Sorts an array recursively using the quicksort method.
+ * @arr: The array to sort.
+ * @left: The leftmost index of the sub-array to sort.
+ * @right: The rightmost index of the sub-array to sort.
+ * @size: The total size of the array (used for printing).
+ */
+void recursive_sort(int *arr, int left, int right, size_t size)
+{
+	int pivot_index;
+
+	if (left < right)
+	{
+		pivot_index = partition(arr, left, right, size);
+		recursive_sort(arr, left, pivot_index - 1, size);
+		recursive_sort(arr, pivot_index + 1, right, size);
+	}
+}
+
+/**
+ * partition - Rearranges the elements of a sub-array around a pivot.
+ * @arr: The array to partition.
+ * @left: The starting index of the sub-array.
+ * @right: The ending index of the sub-array.
+ * @size: The full size of the array (used for printing).
+ * Return: The index of the pivot after partitioning.
+ */
+
+int partition(int *arr, int left, int right, size_t size)
+{
+	int pivot_value = arr[right];
+	int Sindex = left;
+	int j, temp;
+
+	for (j = left; j < right; j++)
+	{
+		if (arr[j] < pivot_value)
+		{
+			if (Sindex != j)
+			{
+				temp = arr[j];
+				arr[j] = arr[Sindex];
+				arr[Sindex] = temp;
+				print_array(arr, size);
+			}
+			Sindex++;
+		}
+	}
+	if (arr[Sindex] != arr[right])
+	{
+		temp = arr[Sindex];
+		arr[Sindex] = arr[right];
+		arr[right] = temp;
+		print_array(arr, size);
+	}
+
+	return (Sindex);
+}
--- sort.h
+++ sort.h
@@ -31,5 +31,8 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void recursive_sort(int *arr, int left, int right, size_t size);
+int partition(int *arr, int left, int right, size_t size);
 
 #endif
